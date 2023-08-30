c7ec98edcca85ceeb02e3b36ffb6a45240731f09
hiepxanh <hiepxanh@gmail.com>
2023-08-30 22:19:53 +0700

    update: footer example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
-import { inject, Component, PLATFORM_ID } from '@angular/core';
+import { ElementRef, ApplicationRef, createComponent, Input, ChangeDetectorRef, inject, Component, PLATFORM_ID, HostBinding } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-
-import {
-  ElementRef,
-  ApplicationRef,
-  createComponent,
-  Input,
-  ChangeDetectorRef,
-} from '@angular/core';
 import { DOCUMENT, isPlatformServer } from '@angular/common';
 
 @Component({
@@ -61,7 +53,6 @@ export class NgxDynamic {
   }
 }
 
-
 @Component({
   standalone: true,
   selector: 'app-navbar',
@@ -71,20 +62,49 @@ export class NavbarComponent {
   @Input() data!: any;
 }
 
+
+@Component({
+  standalone: true,
+  selector: 'app-footer',
+  template: `
+  Footer should not empty <p [innerHTML]="content"> footer here </p>
+  `,
+  styles: [`
+    :host { display: block; }
+  `]
+})
+export class FooterComponent {
+  platformId = inject(PLATFORM_ID);
+  isServer = isPlatformServer(this.platformId);
+  elementRef = inject(ElementRef);
+  @HostBinding('attr.id') id!: string;
+  content!: string;
+  ngOnInit() {
+    if (this.isServer) {
+      // not work: the p tag will not update innerHTML
+      this.content = 'footer content changed';
+      // not work: the footer will not have id attribute
+      this.id = 'id: server';
+      // work: title display if you hover on footer content
+      this.elementRef.nativeElement.setAttribute("title", `platform: ${this.platformId}`);
+    }
+  }
+}
+
+
+
 @Component({
   selector: 'app-root',
-  template: `<p>app-root: {{ platform }}</p>
-    <ngx-dynamic
-      [content]="content"
-      [context]="context"
-      [componentData]="componentData"
-    ></ngx-dynamic>`,
-  imports: [NgxDynamic],
+  template: `<p>app-root: {{ platformId }}</p>
+    <ngx-dynamic [content]="content" [context]="context" [componentData]="componentData"></ngx-dynamic>
+    <app-footer></app-footer>
+    `,
+  imports: [NgxDynamic, FooterComponent],
   standalone: true,
 })
 export class AppComponent {
-  platform = inject(PLATFORM_ID);
-  context = { data: this.platform };
+  platformId = inject(PLATFORM_ID);
+  context = { data: this.platformId };
   content = `<app-navbar></app-nabar>`;
   componentData = {
     component: NavbarComponent,
